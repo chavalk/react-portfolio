2c33df7220be753a8d7c20c1a70fdabb5668cc34
chavalk <chavalk@hotmail.com>
2021-03-20 21:50:08 -0500

    Add nav bar for pages in app.js, remove all boiler plate code from app.css, change page title in index.html, install bootstrap, and create pages folder with Home.js.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import './App.css';
 import React, { Component } from 'react';
 import { HashRouter, Route, Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <HashRouter basename='/'>
       <div>
-        <ul>
-          <li><Link to='/'>Home</Link></li>
-          <li><Link to='/about'>About</Link></li>
+        <ul className="nav justify-content-center pt-2 pb-2" id="list" style={{backgroundColor: "#06224A"}}>
+          <li className="nav-item">
+            <Link to='/' className="nav-link" style={{color: "#FFFFFF"}}>Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link to='/about' className="nav-link" style={{color: "#FFFFFF"}}>About</Link>
+          </li>
+          <li className="nav-item">
+            <Link to='/portfolio' className="nav-link" style={{color: "#FFFFFF"}}>Portfolio</Link>
+          </li>
+          <li className="nav-item">
+            <Link to='/contact' className="nav-link" style={{color: "#FFFFFF"}}>Contact</Link>
+          </li>
         </ul>
-        <hr />
         <Route exact path='/' component={Home} />
         <Route exact path='/about' component={About} />
       </div>
