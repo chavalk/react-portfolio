0367b5385af93cc17f95a07265d490022c0497ca
chavalk <chavalk@hotmail.com>
2021-03-22 12:45:45 -0500

    Add images folder for project screen shots and change code in portfolio.js to test image is working.

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,25 +1,26 @@
 import React from 'react';
 import './style.css';
+import pillBox from '../images/pill-box.png';
 
 const Portfolio = () => {
     return (
         <div className="container" id="container-portfolio">
             <div className="row">
                 <div className="col-md-2">
-                    <div className="col-md-8">
-                        <h1>Portfolio</h1>
-                        <p id="p-portfolio">Below are some of the projects I've worked on thus far.</p>
-                        <div className="row">
-                            <div className="col-md-6">
+                </div>
+                <div className="col-md-8">
+                    <h1>Portfolio</h1>
+                    <p id="p-portfolio">Below are some of the projects I've worked on thus far.</p>
+                    <div className="row">
+                        <div className="col-md-6">
 
-                                <div className="card mb-3 text-center" style={{width: "100%"}}>
-                                    <img src="./images/pill-box.png" className="card-img-top" alt="Website"></img>
-                                        <div className="card-body">
-                                            <h5 className="card-title">Pill Box</h5>
-                                            <p className="card-text">Application to help caretakers efficiently manage their patients' medication.</p>
-                                            <a href="https://infinite-mesa-67411.herokuapp.com/" target="_blank" rel="noreferrer" className="btn btn-dark">See it Live</a>
-                                            <a href="https://github.com/chavalk/Pill-Box" target="_blank" rel="noreferrer" className="btn btn-dark">View Code</a>
-                                        </div>
+                            <div className="card mb-3 text-center" style={{ width: "100%" }}>
+                                <img src={pillBox} className="card-img-top" alt="Website"></img>
+                                <div className="card-body">
+                                    <h5 className="card-title">Pill Box</h5>
+                                    <p className="card-text">Application to help caretakers efficiently manage their patients' medication.</p>
+                                    <a href="https://infinite-mesa-67411.herokuapp.com/" target="_blank" rel="noreferrer" className="btn btn-dark mr-1">See it Live</a>
+                                    <a href="https://github.com/chavalk/Pill-Box" target="_blank" rel="noreferrer" className="btn btn-dark">View Code</a>
                                 </div>
                             </div>
                         </div>
