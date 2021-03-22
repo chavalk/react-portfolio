e9717ef2325c24cd67d199345a3c99e1fe1172a9
chavalk <chavalk@hotmail.com>
2021-03-22 18:54:33 -0500

    Add code to bring in data from json file and pass it to component.

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './style.css';
 import Project from '../components/Project';
+import projects from '../projects.json';
 
 const Portfolio = () => {
     return (
@@ -12,7 +13,13 @@ const Portfolio = () => {
                     <h1>Portfolio</h1>
                     <p id="p-portfolio">Below are some of the projects I've worked on thus far.</p>
                     <div className="row">
-                        <Project />
+                        <Project 
+                            image={projects[0].image}
+                            title={projects[0].title}
+                            description={projects[0].description}
+                            deployed={projects[0].deployed}
+                            repo={projects[0].repo}
+                        />
                     </div>
                 </div>
             </div>
