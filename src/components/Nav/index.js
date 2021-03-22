2804bc824c4c688a971b49247f1b7811529111d2
chavalk <chavalk@hotmail.com>
2021-03-21 20:15:13 -0500

    Change href in nav bar to work with react router.

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -4,7 +4,7 @@ function Nav() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark" style={{backgroundColor: "rgba(0,0,0,0.8)"}}>
         <div className="container-fluid">
-          <a className="navbar-brand" href="index.html">Jose Garcia</a>
+          <a className="navbar-brand" href="/">Jose Garcia</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
@@ -14,10 +14,10 @@ function Nav() {
                 <a className="nav-link active" aria-current="page" href="index.html">About</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="portfolio.html">Portfolio</a>
+                <a className="nav-link" href="/portfolio">Portfolio</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="contact.html">Contact</a>
+                <a className="nav-link" href="/contact">Contact</a>
               </li>
             </ul>
           </div>
