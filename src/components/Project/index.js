7365cc2e32c0d6c4eefc68ea8443093667efa9e4
chavalk <chavalk@hotmail.com>
2021-03-22 19:08:01 -0500

    Change image src to hosted image in own domain to make it easier to reference it in json file.

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import pillBox from '../../images/pill-box.png';
 
-const Project = () => {
+const Project = (props) => {
     return (
         <div className="col-md-6">
             <div className="card mb-3 text-center" style={{ width: "100%" }}>
-                <img src={pillBox} className="card-img-top" alt="Website"></img>
+                <img src={props.image} className="card-img-top" alt="Website"></img>
                 <div className="card-body">
-                    <h5 className="card-title">Pill Box</h5>
-                    <p className="card-text">Application to help caretakers efficiently manage their patients' medication.</p>
-                    <a href="https://infinite-mesa-67411.herokuapp.com/" target="_blank" rel="noreferrer" className="btn btn-dark mr-1">See it Live</a>
-                    <a href="https://github.com/chavalk/Pill-Box" target="_blank" rel="noreferrer" className="btn btn-dark">View Code</a>
+                    <h5 className="card-title">{props.title}</h5>
+                    <p className="card-text">{props.description}</p>
+                    <a href={props.deployed} target="_blank" rel="noreferrer" className="btn btn-dark mr-1">See it Live</a>
+                    <a href={props.repo} target="_blank" rel="noreferrer" className="btn btn-dark">View Code</a>
                 </div>
             </div>
         </div>
