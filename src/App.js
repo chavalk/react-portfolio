872bedaaf307588bbcf3c6ec803edbff79fc17e5
chavalk <chavalk@hotmail.com>
2021-03-22 19:32:26 -0500

    Change home.js to about.js for better readability.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Nav from './components/Nav';
-import Home from './pages/Home';
+import About from './pages/About';
 import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact';
 import Footer from './components/Footer';
@@ -14,7 +14,7 @@ function App() {
       <Router>
         <Nav />
         <Switch>
-          <Route exact path='/' component={Home} />
+          <Route exact path='/' component={About} />
           <Route exact path='/portfolio' component={Portfolio} />
           <Route exact path='/contact' component={Contact} />
         </Switch>
