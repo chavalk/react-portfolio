caafddeab5a4e4cc395a8071464b75a70bc420d1
chavalk <chavalk@hotmail.com>
2021-03-22 23:18:18 -0500

    Change code to make nav bar fixed.

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,29 +1,29 @@
 import React from 'react';
 
 function Nav() {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-dark" style={{backgroundColor: "rgba(0,0,0,0.8)"}}>
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/">Jose</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <a className="nav-link" aria-current="page" href="/">About</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/portfolio">Portfolio</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/contact">Contact</a>
-              </li>
-            </ul>
-          </div>
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark fixed-top" style={{ backgroundColor: "rgba(0,0,0,0.8)" }}>
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">Jose</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" aria-current="page" href="/">About</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/portfolio">Portfolio</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/contact">Contact</a>
+            </li>
+          </ul>
         </div>
+      </div>
     </nav>
-    );
+  );
 }
 
 export default Nav;
