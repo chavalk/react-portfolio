cf9f939e7069260238ac54227c294e4069847e1c
chavalk <chavalk@hotmail.com>
2021-04-20 20:19:11 -0500

    Modify nav bar and page title in index.html.

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -3,22 +3,21 @@ import { Link } from 'react-router-dom';
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark fixed-top" style={{ backgroundColor: "rgba(0,0,0,0.8)" }}>
+    <nav className="navbar navbar-expand-lg fixed-top" style={{ backgroundColor: "rgba(0,0,0,0.8)" }}>
       <div className="container-fluid">
-        <Link to='/' className='navbar-brand'>Jose</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <Link to='/' className='nav-link'>About</Link>
+              <Link to='/' className='nav-link' style={{ color: "white" }}>Home</Link>
             </li>
             <li className="nav-item">
-              <Link to='/portfolio' className='nav-link'>Portfolio</Link>
+              <Link to='/portfolio' className='nav-link' style={{ color: "white" }}>Portfolio</Link>
             </li>
             <li className="nav-item">
-              <Link to='/contact' className='nav-link'>Contact</Link>
+              <Link to='/contact' className='nav-link' style={{ color: "white" }}>Contact</Link>
             </li>
           </ul>
         </div>
