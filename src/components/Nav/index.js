901df6bfa3571f2a2f495e2980ef92d81ed86807
chavalk <chavalk@hotmail.com>
2021-04-23 23:55:50 -0500

    Fix class for className in nav bar.

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -5,7 +5,7 @@ function Nav() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark fixed-top" style={{ backgroundColor: "rgba(0,0,0,0.8)" }}>
       <div className="container-fluid">
-        <Link class="navbar-brand" to="/"></Link>
+        <Link className="navbar-brand" to="/"></Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
