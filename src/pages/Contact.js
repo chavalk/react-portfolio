3330828bdd0d8c077e66c9cbf592bbe417abc8ae
chavalk <chavalk@hotmail.com>
2021-04-24 01:00:10 -0500

    Change layout of contact page.

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,13 +3,17 @@ import './style.css';
 
 const Contact = () => {
     return (
-        <div className="container" id="container-portfolio">
+        <div className="container" id="container-contact">
             <div className="row">
                 <div className="col-md-3">
                 </div>
                 <div className="col-md-6">
-                    <h1 id="h1-contact">Contact</h1>
-                    <p id="p-portfolio">Feel free to reach me by email at <a href="mailto:chavalk@hotmail.com" id="email">chavalk@hotmail.com</a> or by phone at <a href="tel:+1-210-782-7463" id="phone">210-782-7463</a>.</p>
+                    <h1 id="h1-contact">Ready to collaborate?</h1>
+                    <p id="p-contact">I'm always open to new projects to collaborate on, so if you're in need of a full-stack JavaScript developer, please get in touch!</p>
+                    <h3 id="h3-contact">Email:</h3>
+                    <a href="mailto:chavalk@hotmail.com" id="email-contact">chavalk@hotmail.com</a>
+                    <h3 id="h3-contact">Phone:</h3>
+                    <a href="tel:+1-210-782-7463" id="phone-contact">210-782-7463</a>
                 </div>
                 <div className="col-md-3">
                 </div>
